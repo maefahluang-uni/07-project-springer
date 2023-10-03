5b3eaf376f5d4e6f3303ca384a06168bf260a3f8
Siwasith <118959965+Siwasith@users.noreply.github.com>
2023-10-03 10:39:32 +0700

    add user

---
+++ user-service/src/main/java/user/service/userservice/UserDTO.java
@@ -0,0 +1,40 @@
+package user.service.userservice;
+
+public class UserDTO {
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String phone;
+
+    
+    public UserDTO() {
+    }
+    
+    public String getFirstname() {
+        return firstname;
+    }
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+    
+}
