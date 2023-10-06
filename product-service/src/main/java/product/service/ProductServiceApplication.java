075635860eb484dacf4f190c4e9d47a2535d6ea9
Siwasith <118959965+Siwasith@users.noreply.github.com>
2023-10-06 11:06:39 +0700

    add some html (product)

--- product-service/src/main/java/product/service/ProductServiceApplication.java
+++ product-service/src/main/java/product/service/ProductServiceApplication.java
@@ -3,6 +3,9 @@ package product.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication 
 @EnableEurekaClient
@@ -11,6 +14,19 @@ public class ProductServiceApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				// TODO: make sure the port of web front is correct.
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
+			}
+		};
 	}
 
 	
--- user/src/main/java/user/service/user/UserApplication.java
+++ user/src/main/java/user/service/user/UserApplication.java
@@ -3,6 +3,7 @@ package user.service.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,6 +11,17 @@ public class UserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				// TODO: make sure the port of web front is correct.
+				registry.addMapping("/users").allowedOrigins("http://localhost:8080");
+			}
+		};
 	}
 
 }
